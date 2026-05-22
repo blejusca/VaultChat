9f9b8ab7872989d3208f98c0713662671b7ab1a2
blejusca <vasi37v@yahoo.com>
2026-05-22 21:57:08 +0300

    v0.9.2-stable - icon, onboarding, splash, replay fix, release signing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,14 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+val keyPropertiesFile = rootProject.file("key.properties")
+val keyProperties = Properties()
+keyProperties.load(keyPropertiesFile.inputStream())
 
 android {
     namespace = "com.vaultchat.app"
@@ -26,8 +32,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keyProperties["keyAlias"] as String
+            keyPassword = keyProperties["keyPassword"] as String
+            storeFile = file(keyProperties["storeFile"] as String)
+            storePassword = keyProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
